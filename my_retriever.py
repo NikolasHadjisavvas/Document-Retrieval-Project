19505555f7a975fd25b60b65f45ec20e473382d0
Nicolas Hadjisavvas <nicolashadjisavvas@gmail.com>
2021-10-28 21:37:09 +0100

    Started implementing my_retriever.py, completed 2 methods

--- my_retriever.py
+++ my_retriever.py
@@ -1,3 +1,4 @@
+import math
 
 class Retrieve:
     
@@ -7,6 +8,7 @@ class Retrieve:
         self.index = index
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
+
         
     def compute_number_of_documents(self):
         self.doc_ids = set()
@@ -18,6 +20,27 @@ class Retrieve:
     # represented as a list of preprocessed terms). Returns list 
     # of doc ids for relevant docs (in rank order).
     def for_query(self, query):
+        #print("QUERY:",query)
         return list(range(1,11))
 
+
+    # Method for commputing the inverted document frequency of 
+    # each term in the collection.
+    # idf of a word(w) in a document collection(D) is: log(|D|/dfw) --> log of the total number of documents in collection over the number of documents containing w.
+    def compute_idf_for_all_terms(self, index):
+
+        all_terms = list(index.keys())
+        total_doc_number = self.compute_number_of_documents()  
+
+        idfs = {}
+        for term in all_terms:
+            idfs[term] = math.log(total_doc_number/self.compute_doc_freq(term))
+                
+        return idfs
+
+
+    # Method for returning the document frequency of a term/word specified as a parameter.
+    def compute_doc_freq(self, word):
+        dfw = len(self.index[word])
+        return dfw
 
