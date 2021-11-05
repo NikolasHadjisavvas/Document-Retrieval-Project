726b47ec017934ab45d604dafadf33bb54a475a6
Nicolas Hadjisavvas <nicolashadjisavvas@gmail.com>
2021-11-05 21:32:50 +0000

    'update'

--- my_retriever.py
+++ my_retriever.py
@@ -1,4 +1,6 @@
 import math
+import json
+import collections
 
 class Retrieve:
     
@@ -9,10 +11,12 @@ class Retrieve:
         self.term_weighting = term_weighting
         self.num_docs = self.compute_number_of_documents()
         self.ALL_TERMS = list(self.index.keys())
-        self.idfs = self.compute_idf_for_all_terms(self.index)
+        if self.term_weighting=='tfidf':
+            self.idfs = self.compute_idf_for_all_terms()
+        self.all_document_vectors,self.all_document_vec_lengths=self.construct_all_document_vectors()
+        
+        print(self.all_document_vec_lengths)
 
-        query=['what', 'articles', 'exist', 'which', 'deal', 'with', 'tss', 'time', 'sharing', 'system', 'an', 'operating', 'system', 'for', 'ibm', 'computers']
-        print(self.compute_cos(query,2020))
 
     def compute_number_of_documents(self):
         self.doc_ids = set()
@@ -20,63 +24,51 @@ class Retrieve:
             self.doc_ids.update(self.index[term])
         return len(self.doc_ids)
 
-    # Method performing retrieval for a single query (which is 
-    # represented as a list of preprocessed terms).Returns list 
-    # of doc ids for relevant docs (in rank order).
-    def for_query(self, query):
-        #print("QUERY:",query)
-        return list(range(1,11))
-
 
     # Method for commputing the inverted document frequency of 
     # each term in the collection.
     # idf of a word(w) in a document collection(D) is: log(|D|/dfw) --> log of the total number of documents in collection over the number of documents containing w.
-    def compute_idf_for_all_terms(self, index):
+    def compute_idf_for_all_terms(self):
         total_doc_number = self.compute_number_of_documents()  
         idfs = {}
         for term in self.ALL_TERMS:
-            idfs[term] = math.log(total_doc_number/self.compute_doc_freq(term))
-
+            idfs[term] = math.log10(total_doc_number/self.compute_doc_freq(term))
         return idfs
 
+    #Compute term weighting accarding to the selected scheme, at the same time build the document vectors and compute their length.
+    def construct_all_document_vectors(self):
+        all_doc_vectors={}
+        doc_vector_lengths={}
 
-    # Takes a term and a document
-    # Returns tfidf of that term for all documents
-    # Note: Expensive to compute for all documents(because later I will need to index all values as well), 
-    # many of them are 0 anyway, I can compute and store only the non zero,and for the vector length computation, check if
-    # doc is present in the dict of all tfidfs for the term, if not present then we know the tfidf is 0.
-    def compute_tdidfs_for_document_terms(self, document):
+        for term in self.ALL_TERMS:
+            for doc in range(1,self.num_docs+1): 
+                if doc in self.index[term]:#if doc contains term
+                    tf = self.index[term][doc] 
+                    if self.term_weighting=='tfidf':
+                        idf = self.idfs[term]
+                        if doc not in all_doc_vectors:
+                            all_doc_vectors[doc] = {term:tf * idf}
+                            doc_vector_lengths[doc]= (tf * idf)**2
+                        else:
+                            all_doc_vectors[doc][term] = tf * idf
+                            doc_vector_lengths[doc]+= (tf * idf)**2
+                    elif self.term_weighting =='tf':
+                        if doc not in all_doc_vectors:
+                            all_doc_vectors[doc] = {term:tf}
+                            doc_vector_lengths[doc]= tf**2
+                        else:
+                            all_doc_vectors[doc][term] = tf
+                            doc_vector_lengths[doc]+= tf**2
 
-        words = self.getWordsInDoc(document)
-        tfidfs_of_document = {}
-        for word in words:
-            tf = words.count(word)
-            idf=self.idfs[word]
-            tfidfs_of_document[word] = tf*idf
-        
-        return tfidfs_of_document
+                        
+        doc_vector_lengths2={}
+
+        for key,value in doc_vector_lengths.items():
+            doc_vector_lengths2[key] = math.sqrt(value)
+
+        return all_doc_vectors,doc_vector_lengths2
 
     
-    def compute_tdidfs_for_query_terms(self, query):
-
-        tfidfs_of_query={}
-        for word in query:
-            tf = query.count(word)
-            idf=self.idfs[word]
-            tfidfs_of_query[word] = tf*idf
-        
-        return tfidfs_of_query
-
-
-    def getWordsInDoc(self, document):
-        arr=[]
-        items=self.index.items()
-        for item in items:
-            docs_with_word=item[1].keys()
-            if document in docs_with_word:
-                arr.append(item[0])
-        return arr
-
     # Takes a term and a document.
     # Returns 1 if the term is present in the document, else returns 0.
     def compute_binary_weighting(self, term, document):
@@ -94,50 +86,102 @@ class Retrieve:
         #test for commit
         return dfw
 
-    
-    def construct_vector_for_doc(self, document):
-
-        #if we use tfidf weghting, use the tfidf method,if useing binary,use the binary version of the method etc.
-        vector=self.compute_tdidfs_for_document_terms(document)
+    #Need to confirm that this is correct
+    def compute_tfidfs_for_query_terms(self, query):
+        tfidfs_of_query={}
+        for word in query:
+            tf = query.count(word)
+            if word in self.idfs:
+                idf=self.idfs[word]
+            else:
+                idf = 0
+            tfidfs_of_query[word] = tf*idf
         
-        return vector
+        return tfidfs_of_query
 
-    
+
     def construct_vector_for_query(self, query):
 
-        vector = self.compute_tdidfs_for_query_terms(query)
-        
+        if self.term_weighting=='tfidf':
+            vector = self.compute_tfidfs_for_query_terms(query)
+        if self.term_weighting=='tf':
+            vector={}
+            for word in query:
+                vector[word]=query.count(word)
+
         return vector
 
+
+    def compute_vector_length(self,vector):
+        sum=0
+        values=vector.values()
+        for i in values:
+            sum += i*i
+
+        length = math.sqrt(sum)
     
-    def compute_numerator_of_cos(self, query,document):
-    
-        doc_vector = self.construct_vector_for_doc(document)
-        query_vector = self.construct_vector_for_query(query)
+        return length
 
-        numer = 0
-        for word in query:
-            if word in list(doc_vector.keys()):
-                numer+=query_vector[word] * doc_vector[word]
 
-        return numer
+    # Method performing retrieval for a single query (which is 
+    # represented as a list of preprocessed terms).Returns list 
+    # of doc ids for relevant docs (in rank order).
+    def for_query(self, query):
+            query_vector = self.construct_vector_for_query(query)
+            doc_vectors= self.all_document_vectors
 
-    def compute_denominator_of_cos(self,query,document):
-        doc_vector = self.construct_vector_for_doc(document)
-        query_vector = self.construct_vector_for_query(query)  
+            scores={}
 
-        denom=0
-        sumq=0
-        sumd=0
+            candidates=set()
+            for word in query:
+                if word in self.index:
+                    candidates.update(self.index[word].keys())
 
-        for word in query:
-            sumq += query_vector[word]*query_vector[word]
-        for word in doc_vector.keys():
-            sumd+=doc_vector[word]*doc_vector[word]
 
-        denom = math.sqrt(sumq) * math.sqrt(sumd)
+            for i in candidates:
+                #compute numerator
+                numer = 0
+                for word in list(query_vector.keys()):
+                    if word in list(doc_vectors[i].keys()):
+                        numer+=query_vector[word] * doc_vectors[i][word]
+                
+                denom = self.all_document_vec_lengths[i] #do this for all docs in construct_all_doc_vectors
+                
+                scores[i] = numer/denom
 
-        return denom
+            sorted_score_values = sorted(scores.items(), key=lambda kv: kv[1], reverse=True)
+            sorted_scores=collections.OrderedDict(sorted_score_values)
+            
+            return list(sorted_scores.keys())[:10]
 
-    def compute_cos(self,query,document):
-        return self.compute_numerator_of_cos(query,document)/self.compute_denominator_of_cos(query,document)
+    """ #JUST FOR TESTING
+        def forQuery(self, query):
+            query_vector = self.construct_vector_for_query(query)
+            doc_vectors= self.all_document_vectors
+
+
+            scores={}
+
+            candidates=set()
+            for word in query:
+                if word in self.index:
+                    candidates.update(self.index[word].keys())
+
+            for i in candidates:
+                #compute numerator
+                numer = 0
+                for word in list(query_vector.keys()):
+                    if word in list(doc_vectors[i].keys()):
+                        numer+=query_vector[word] * doc_vectors[i][word]
+                        print('Mult. '+str(query_vector[word])+' of query with '+str(doc_vectors[i][word])+' of vector '+str(i), query_vector[word] * doc_vectors[i][word])
+                
+                
+                denom = self.compute_vector_length(doc_vectors[i])
+                
+                scores[i] = numer/denom
+                print('End for doc'+str(i)+' numerator is '+str(scores[i]))
+
+            sorted_score_values = sorted(scores.items(), key=lambda kv: kv[1], reverse=True)
+            sorted_scores=collections.OrderedDict(sorted_score_values)
+            
+            return list(sorted_scores.keys())[:10]"""
