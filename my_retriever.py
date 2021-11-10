03be165a93bbed0c2495df3ff08859baa512320d
Nicolas Hadjisavvas <nicolashadjisavvas@gmail.com>
2021-11-10 09:51:49 +0000

    Update

--- my_retriever.py
+++ my_retriever.py
@@ -1,5 +1,4 @@
 import math
-import json
 import collections
 
 class Retrieve:
@@ -15,7 +14,6 @@ class Retrieve:
             self.idfs = self.compute_idf_for_all_terms()
         self.all_document_vectors,self.all_document_vec_lengths=self.construct_all_document_vectors()
         
-
 
     def compute_number_of_documents(self):
         self.doc_ids = set()
@@ -34,94 +32,86 @@ class Retrieve:
             idfs[term] = math.log10(total_doc_number/self.compute_doc_freq(term))
         return idfs
 
-    #Compute term weighting accarding to the selected scheme, at the same time build the document vectors and compute their length.
+    #Compute term weighting according to the selected scheme, at the same time build the document vectors and compute their length.
     def construct_all_document_vectors(self):
         all_doc_vectors={}
-        doc_vector_lengths={}
-
+        doc_vector_element_summations={} #Initialise dict with the element wise summation of a vector.{doc vector->sum}.We use that later to apply sqrt and get the lenghts.
+ 
+        #Iterate over all terms in index
         for term in self.ALL_TERMS:
+            #For each document in the collection, compute the chosen term weighting for its terms.
             for doc in range(1,self.num_docs+1): 
-                if doc in self.index[term]:#if doc contains term
-                    tf = self.index[term][doc] 
+                if doc in self.index[term]: #if doc contains term(thus, we drop all the vector elements that are going to be 0)
+                    tf = self.index[term][doc] #Compute the tf of each term in each document(just by indexing the index)
                     if self.term_weighting=='tfidf':
-                        idf = self.idfs[term]
-                        if doc not in all_doc_vectors:
+                        idf = self.idfs[term] # Get the corresponding precomputed idf. 
+
+                        # Since we take each term in index one by one, our vectors are going to be updated step by step in each loop of the outer for.
+                        # Here we initialize the vector in case we found a term belonging to a specific document for the 1st time.
+                        if doc not in all_doc_vectors: 
                             all_doc_vectors[doc] = {term:tf * idf}
-                            doc_vector_lengths[doc]= (tf * idf)**2
-                        else:
+                            doc_vector_element_summations[doc]= (tf * idf)**2
+                        else: # In case the vector has already been initialised, just update its content.
                             all_doc_vectors[doc][term] = tf * idf
-                            doc_vector_lengths[doc]+= (tf * idf)**2
-                    elif self.term_weighting =='tf':
+                            doc_vector_element_summations[doc]+= (tf * idf)**2
+                    elif self.term_weighting =='tf': # Same as above but with tf
                         if doc not in all_doc_vectors:
                             all_doc_vectors[doc] = {term:tf}
-                            doc_vector_lengths[doc]= tf**2
+                            doc_vector_element_summations[doc]= tf**2
                         else:
                             all_doc_vectors[doc][term] = tf
-                            doc_vector_lengths[doc]+= tf**2
+                            doc_vector_element_summations[doc]+= tf**2
+                    elif self.term_weighting=='binary': # Same as above with tf
+                        if doc not in all_doc_vectors:
+                            all_doc_vectors[doc] = {term:1} # If term is in document, then weight in vector is 1.
+                            doc_vector_element_summations[doc]= 1**2
+                        else:
+                            all_doc_vectors[doc][term] = 1
+                            doc_vector_element_summations[doc]+= 1**2                      
 
-                        
-        doc_vector_lengths2={}
+        doc_vector_lengths={} #Initialise a dictionary {doc vector->doc length} to store vector lenghts.
 
-        for key,value in doc_vector_lengths.items():
-            doc_vector_lengths2[key] = math.sqrt(value)
+        # Compute sqrt of element sums of vector elements to get lengths.
+        for key,value in doc_vector_element_summations.items():
+            doc_vector_lengths[key] = math.sqrt(value)
 
-        return all_doc_vectors,doc_vector_lengths2
+        return all_doc_vectors,doc_vector_lengths
 
     
-    # Takes a term and a document.
-    # Returns 1 if the term is present in the document, else returns 0.
-    def compute_binary_weighting(self, term, document):
-        term_in_index = self.index[term]    #{1234:1, 1432:2}
-        docs = list(term_in_index.keys())
-        if document in docs:
-            return 1
-        else:
-            return 0 
-
 
     # Method for returning the document frequency of a term/word specified as a parameter.
     def compute_doc_freq(self, word):
         dfw = len(self.index[word])
-        #test for commit
         return dfw
 
     #Need to confirm that this is correct
     def compute_tfidfs_for_query_terms(self, query):
-        tfidfs_of_query={}
+        tfidfs_of_query={} # Initialise dict {query term->tfidf}
         for word in query:
-            tf = query.count(word)
-            if word in self.idfs:
+            tf = query.count(word) 
+            if word in self.idfs: #If term is present in the document collection.Get the pre computed idf.
                 idf=self.idfs[word]
-            else:
-                idf = 0
-            tfidfs_of_query[word] = tf*idf
+                tfidfs_of_query[word] = tf*idf
         
         return tfidfs_of_query
 
 
     def construct_vector_for_query(self, query):
 
-        if self.term_weighting=='tfidf':
+        if self.term_weighting=='tfidf': #If  we use tfidf, just use the above method.
             vector = self.compute_tfidfs_for_query_terms(query)
-        if self.term_weighting=='tf':
+        if self.term_weighting=='tf': #If we use tf, just use the tf of query term in the query(only if the term also appears in index.)
             vector={}
             for word in query:
                 if word in self.index:
                     vector[word]=query.count(word)
-
+        if self.term_weighting=='binary': #If we use binary, just use the binary weight of query term in the query(only if the term also appears in index.)
+            vector={}
+            for word in query:
+                if word in self.index:
+                    vector[word] = 1
 
         return vector
-
-
-    def compute_vector_length(self,vector):
-        sum=0
-        values=vector.values()
-        for i in values:
-            sum += i*i
-
-        length = math.sqrt(sum)
-    
-        return length
 
 
     # Method performing retrieval for a single query (which is 
@@ -130,14 +120,13 @@ class Retrieve:
     def for_query(self, query):
             query_vector = self.construct_vector_for_query(query)
             doc_vectors= self.all_document_vectors
-
             scores={}
 
+            # Get all documents containing at least one query term.
             candidates=set()
             for word in query:
                 if word in self.index:
                     candidates.update(self.index[word].keys())
-
 
             for i in candidates:
                 #compute numerator
@@ -146,7 +135,7 @@ class Retrieve:
                     if word in list(doc_vectors[i].keys()):
                         numer+=query_vector[word] * doc_vectors[i][word]
                 
-                denom = self.all_document_vec_lengths[i] #do this for all docs in construct_all_doc_vectors
+                denom = self.all_document_vec_lengths[i]
                 
                 scores[i] = numer/denom
 
@@ -154,4 +143,4 @@ class Retrieve:
             sorted_scores=collections.OrderedDict(sorted_score_values)
             
             return list(sorted_scores.keys())[:10]
-            
+
